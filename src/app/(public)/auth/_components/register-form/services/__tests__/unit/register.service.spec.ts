518ed69d5938d70fe1f0b0464e915f1af611bb6d
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2025-02-08 15:28:21 -0300

    test(auth): Add register unit tests

---
+++ src/app/(public)/auth/_components/register-form/services/__tests__/unit/register.service.spec.ts
@@ -0,0 +1,27 @@
+import { fetcher } from '@/utils/fetcher';
+import { registerService } from '../../register.service';
+
+jest.mock('@/utils/fetcher');
+
+describe('registerService unit tests', () => {
+	it('should register user', async () => {
+		const mockResponse = { data: { success: true } };
+
+		(fetcher as jest.Mock).mockResolvedValue(mockResponse);
+
+		const input = {
+			email: 'email@test.com',
+			name: 'test name',
+			password: '12345678',
+		};
+
+		const output = await registerService.register(input);
+
+		expect(fetcher).toHaveBeenCalledTimes(1);
+		expect(fetcher).toHaveBeenCalledWith('/user/v1/register', {
+			method: 'POST',
+			body: input,
+		});
+		expect(output).toEqual(mockResponse);
+	});
+});
