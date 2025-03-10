56cb8509001eadd60951839a4c54686604355c8c
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2025-03-10 14:49:22 -0300

    chore: Replace helper text prop

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -26,10 +26,10 @@ const Input = ({
 			<div className="relative flex items-center">
 				{leftContent}
 				<input
-					data-helperText={!!helperText}
+					data-helpertext={!!helperText}
 					type={type}
 					className={cn(
-						'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm data-[helperText=true]:border-error',
+						'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm data-[helpertext=true]:border-error',
 						className,
 					)}
 					ref={ref}
